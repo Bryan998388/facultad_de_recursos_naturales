e30846be8f560fbbc4d200ce59ac50d358282e25
Bryan998388 <tunianderson575@gmail.com>
2024-11-25 16:24:31 -0500

    codif

--- src/main/java/com/mycompany/facultad_de_recursos_naturales/Facultad_de_recursos_naturales.java
+++ src/main/java/com/mycompany/facultad_de_recursos_naturales/Facultad_de_recursos_naturales.java
@@ -12,5 +12,6 @@ public class Facultad_de_recursos_naturales {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Me gusta la materia: ");
     }
 }
