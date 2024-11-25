6254119494d982b351949f4301699c67d986cae4
Bryan998388 <tunianderson575@gmail.com>
2024-11-25 16:22:44 -0500

    tarea_2

---
+++ src/main/java/com/mycompany/facultad_de_recursos_naturales/Facultad_de_recursos_naturales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.facultad_de_recursos_naturales;
+
+/**
+ *
+ * @author speyc
+ */
+public class Facultad_de_recursos_naturales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
